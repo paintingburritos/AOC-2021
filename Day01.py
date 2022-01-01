0cb37268a086dabd540e8116bbd7ddb9cffc6039
paintingburritos <45707061+paintingburritos@users.noreply.github.com>
2021-12-31 19:12:48 -0700

    Create Day01.py

---
+++ Day01.py
@@ -0,0 +1,21 @@
+with open('puzzleInput.txt') as pInput:
+  lines = pInput.readlines()
+lines = [int(i) for i in lines]
+
+# depths: List of depths
+# windowLen: Length of window being summed
+def windowIncrease(depths,windowLen):
+  count = 0
+  for i in range(len(depths)-windowLen):
+    sumA = 0
+    sumB = 0
+    for j in range(windowLen):
+      sumA += depths[i+j]
+      sumB += depths[i+j+1]
+
+    if sumB > sumA:
+      count += 1
+  return count
+
+
+print(windowIncrease(lines,1))
